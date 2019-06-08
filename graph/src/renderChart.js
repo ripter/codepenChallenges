1a112019bb2cc4afca923d41e96eb9e24e729a31
Chris Richards <ripter001@gmail.com>
2019-06-07 18:36:00 -0700

    Adding lines that follow the label.

--- graph/src/renderChart.js
+++ graph/src/renderChart.js
@@ -56,39 +56,56 @@ export function renderChart(data) {
 
 function drawSelected(sel, data) {
   const enter = sel.selectAll('circle').data(data).enter();
+
+  // Draw a line linking the point and label
+  const label = enter.append('g')
+    .call(d3.drag()
+      .on('start', dragstarted)
+      .on('drag', dragged)
+      .on('end', dragended));
+
+  const line = label.append('line')
+    .style('stroke', 'black')
+    .style('stroke-width', 0.01)
+    .attr('x1', r => scaleX(r[1])).attr('y1', r => scaleY(r[2]))
+    .attr('x2', r => scaleX(r[1])).attr('y2', r => scaleY(r[2]));
+
+  // Add the label text
+  const text = label.append('text')
+    .attr('x', r => scaleX(r[1]))
+    .attr('y', r => scaleY(r[2]))
+    .style('font-size', 0.2)
+    .text(r => r[0]);
+
+
   // Add the point
   const point = enter.append('circle')
     .attr('cx', r => scaleX(r[1]))
     .attr('cy', r => scaleY(r[2]))
     .attr('r', 0.05)
     .attr('fill', 'red');
-
-  // Add the label text
-  const label = enter.append('text')
-    .attr('x', r => scaleX(r[1]))
-    .attr('y', r => scaleY(r[2]))
-    .style('font-size', 0.2)
-    .text(r => r[0])
-    .call(d3.drag()
-      .on('start', dragstarted)
-      .on('drag', dragged)
-      .on('end', dragended));
-
-  // Draw a line linking the point and label
 }
 
 
 
 function dragstarted(d) {
-  d3.select(this).raise().classed('active', true);
+  d3.select(this)
+    .raise()
+    .classed('active', true);
 }
 
 function dragged(d) {
-  d3.select(this)
-    .attr('x', d.x = d3.event.x)
-    .attr('y', d.y = d3.event.y);
+  d3.select(this).select('text')
+    .attr('x', d3.event.x)
+    .attr('y', d3.event.y);
+
+  d3.select(this).select('line')
+    .attr('x2', d3.event.x + 0.25)
+    .attr('y2', d3.event.y + 0.05);
 }
 
 function dragended(d) {
-  d3.select(this).classed('active', false);
+  d3.select(this)
+    .classed('active', false)
+    .lower();
 }
