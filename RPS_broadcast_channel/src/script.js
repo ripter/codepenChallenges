89655dcb6ebc7d13e0b563101791de3afd0587d8
ripter <ripter001@gmail.com>
2020-03-15 15:43:14 -0700

    I'm starting to think my problems are from the library.

--- RPS_broadcast_channel/src/script.js
+++ RPS_broadcast_channel/src/script.js
@@ -10,9 +10,10 @@ import {define, useReducer, useEffect} from 'https://unpkg.com/hooked-elements?m
 function gameLogic(prevState, action) {
   const { type, hand, token, payload } = action;
   let state = {...prevState}; // Shallow copy
-  // console.group('gameLogic Loop');
-  // console.log('action', action);
-  // console.log('prevState', prevState);
+  console.group('gameLogic Loop');
+  console.log('action', action);
+  console.log('prevState', prevState);
+  console.log('playerToken', prevState.playerToken);
   // console.log('state.token', state.token);
 
   //
@@ -22,8 +23,7 @@ function gameLogic(prevState, action) {
     Object.assign(state, initGame(payload, token));
   }
   else if (type === 'click') {
-    state.playerHand = hand;
-    state.playerToken = uuidv4();
+    // state.playerHand = hand;
   }
 
   // update share link and saved state.
@@ -35,23 +35,54 @@ function gameLogic(prevState, action) {
   // Update the token
   setToken(turn);
 
-  // console.log('queryParams', queryParams);
-  // console.log('state', state);
-  // console.groupEnd();
+  console.log('turn', turn);
+  console.log('state', state);
+  console.groupEnd();
   return state;
 }
 
 //
 // Returns the starting state based on localStorage and URL Search params
 function initGame(payload, token) {
+  const state = {};
+
+  // Token holds our hand/token
+  if (!token) {
+    state.playerToken = uuidv4();
+    state.playerHand = null;
+  }
+  else {
+    state.playerToken = token.token;
+    state.playerHand = token.hand;
+  }
+
+  // Payload holds opponent hand/token
+  if (payload && payload.token !== state.playerToken) {
+    state.opponentToken = payload.token;
+    state.opponentHand = payload.hand;
+  }
+
+  return state;
+  // console.group('initGame');
+  // console.log('payload', payload);
+  // console.log('token', token);
+  // console.groupEnd();
   // Payload with no Token means we loaded from an opponent link,
   // but we have not taken our turn yet.
-  if (payload && !token) {
-    return {
-      opponentHand: payload.hand,
-      opponentToken: payload.token,
-    };
-  }
+  // if (payload && !token) {
+  //   console.log('Opponent has taken their turn, now take yours!');
+  //   return {
+  //     opponentHand: payload.hand,
+  //     opponentToken: payload.token,
+  //     playerToken: uuidv4(),
+  //   };
+  // }
+  //
+  // if (!payload && !token) {
+  //   return {
+  //     playerToken: uuidv4(),
+  //   };
+  // }
 }
 
 
@@ -82,13 +113,21 @@ define('rps-game', element => {
   render(element, html`
     <h1>Rock Paper Scissors!</h1>
 
+    <h2>Pick your hand!</h2>
     <rps-hand-picker
       selected=${state.playerHand}
       .possibleHands=${state.possibleHands}
       .dispatch=${dispatch}
       />
 
+    <h2>Opponent hand</h2>
     ${ShareLink({payload: state.sharePayload})}
+
+    <rps-hand-picker
+      selected=${state.opponentHand}
+      .possibleHands=${state.possibleHands}
+      />
+
   `);
 });
 
@@ -106,14 +145,11 @@ define('rps-hand-picker', element => {
   </dialog>`);
 });
 
-
-
 //
 // Share/Varifiy Link.
 // This link is shared with the Opponent
 function ShareLink(props) {
   const { payload } = props
-  console.log('ShareLink', props);
   return html`
     <a href=${`${window.location.href}?${payload}`}>Opponent Link!</a>
   `;
@@ -159,16 +195,25 @@ function createPayload(payload) {
 // Returns the stored token object or null
 function getToken() {
   try {
-    return JSON.parse(localStorage.getItem('active-game'));
+    const state = JSON.parse(localStorage.getItem('active-game'));
+    // If it's an empty object, return null instead.
+    if (Object.keys(state).length === 0) {
+      return null;
+    }
+    return state;
   }
   catch (e) {
     return null;
   }
 }
 // Sets the token object in localStorage
+// Set a null token to clear it.
 function setToken(token) {
   try {
-    return localStorage.setItem('active-game', JSON.stringify(token));
+    if (token) {
+      return localStorage.setItem('active-game', JSON.stringify(token));
+    }
+    return localStorage.removeItem('active-game');
   }
   catch (e) {
     // oops, game won't work without localStorage
